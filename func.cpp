77983917c056dd54ca0730cea5618e4b64ec17d9
muazalamri <eng.muazalamri@gmail.com>
2025-10-06 18:05:24 +0300

    edit mul tools

--- func.cpp
+++ func.cpp
@@ -13,13 +13,20 @@ dtype sub(dtype x1, dtype x2) {
         x1[i] = x1[i] - x2[i];
     }
 };
-int calc_index(int* shape[], int* strides[], int* posion[]) {
+int calc_index(int *position, int *strides, int ndim)
+{
     int index = 0;
-    for (int i = 0; i < sizeof(shape)/sizeof(shape[0]); i++)
+    for (int i = 0; i < ndim; i++)
     {
-        index += *posion[i] * *strides[i] * *shape[i];
+        index += position[i] * strides[i];
     }
     return index;
+}
+void compute_strides(int* shape, int* strides, int ndim) {
+    strides[ndim - 1] = 1;
+    for (int i = ndim - 2; i >= 0; i--) {
+        strides[i] = strides[i + 1] * shape[i + 1];
+    }
 }
 template <typename dtype>
 dtype mul(dtype x1, dtype x2) {
