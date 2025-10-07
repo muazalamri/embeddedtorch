5aae2372a92a8fafee6dbff83fd3c93e07de11fc
muazalamri <eng.muazalamri@gmail.com>
2025-10-07 18:53:53 +0300

    update tensor2cpp

--- cpp.py
+++ cpp.py
@@ -1,11 +1,8 @@
 import torch
 def tensor2cpp(tensor,dtype)->str:
-    tensor=torch.flatten(tensor)
-    ndim=len(tensor.shape)
-    if ndim==0:
-        return str(tensor)
-    tensor=str([dtype(i) for i in list(tensor)])
-    return tensor.replace('[','{').replace(']','}')
+    tensor=list(tensor.cpu().flatten().numpy().tolist())
+    return str(tensor).replace('[','{').replace(']','}')
+
 if __name__=="__main__":
     a=torch.randn(3,4,5)
     print(tensor2cpp(a,float))
